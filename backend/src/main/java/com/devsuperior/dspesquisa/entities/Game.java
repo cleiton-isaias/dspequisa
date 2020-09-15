4308f3a5ae741ff1fe55f9ca60390c1befc3c182
cleitonisaias@live.com <cleiton.isaiaslima@cencosud.com.br>
2020-09-15 18:16:54 -0300

    Implementação DTO e Service

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -26,9 +26,11 @@ public class Game implements Serializable {
 	private String title;
 	private Platform platform;
 	
+	
 	@ManyToOne
 	@JoinColumn(name = "genre_id")
 	private Genre genre;
+	
 	
 	@OneToMany(mappedBy = "game")
 	private List<Record> records = new ArrayList<>();
@@ -60,7 +62,7 @@ public class Game implements Serializable {
 		this.title = title;
 	}
 
-	public Platform getPltform() {
+	public Platform getPlatform() {
 		return platform;
 	}
 
