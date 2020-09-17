8db4d12ab4c0dac27aab2abedccfa25d90dae2b3
cleitonisaias@live.com <cleiton.isaiaslima@cencosud.com.br>
2020-09-16 23:59:06 -0300

    Criando navegação com rotas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
+import Routes from './Routes';
+
 // import Hello from './Hello';
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Home/>
+    <Routes />
 
-    </div>
   );
 }
 
