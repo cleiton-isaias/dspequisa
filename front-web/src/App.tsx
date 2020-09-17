82261ec9c7a2684885d6ab194a340e6374d53a6d
cleitonisaias@live.com <cleiton.isaiaslima@cencosud.com.br>
2020-09-16 23:12:09 -0300

    Criando página Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 // import Hello from './Hello';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Home/>
 
     </div>
   );
